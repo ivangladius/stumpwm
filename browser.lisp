dcd53b5e8b1cc251e997490ac71056c27c9495d1
ivangladius <112496431+ivangladius@users.noreply.github.com>
2023-04-15 20:01:46 +0200

    Add files via upload

--- browser.lisp
+++ browser.lisp
@@ -8,11 +8,24 @@
 (defparameter bitcoin "https://www.kraken.com/prices/bitcoin")
 (defparameter jasmy "https://www.kraken.com/prices/jasmycoin")
 
+
+;; ### OLD VERSION ###
+;; (defun select-from-menu-visit-in-browser (list-pairs)
+;;   (run-shell-command (format nil "chromium --new-window ~A"
+;; 			     (when (null (cadr (select-from-menu
+;; 				    (current-screen)
+;; 				    list-pairs)))
+;; 			       (run-commands "abort")))))
+
+
+;; ### NEW VERSION
 (defun select-from-menu-visit-in-browser (list-pairs)
-  (run-shell-command (format nil "chromium --new-window ~A"
-			     (cadr (select-from-menu
-				    (current-screen)
-				    list-pairs)))))
+  (let ((valid-content (cadr (select-from-menu
+		       (current-screen)
+		       list-pairs))))
+    (and valid-content (run-shell-command
+		 (format nil "chromium --new-window ~A" valid-content)))))
+					   
 
 (defcommand fsites () ()
   (select-from-menu-visit-in-browser
